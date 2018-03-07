361432e84f3ebe88737d271fb8ed8f8ad57f49b5
Gab0 <gabriel_scf@hotmail.com>
2018-03-07 17:29:03 -0300

    code fix

--- gitltr.py
+++ gitltr.py
@@ -10,17 +10,20 @@ Q=repo.index.diff(repo.head.commit)
 C=repo.index.diff('HEAD')
 N=repo.head.commit.diff()
 
-W = repo.index.diff(None)
+FileDiffs = repo.index.diff(None)
 
 DIFFS = repo.git.diff(repo.head.commit.tree)
 #print(DIFFS)
 
-for F in W:
+for F in FileDiffs:
     File = F.b_path
-    AA = list(repo.iter_commits(paths=File))
+    commitsOnFile = list(repo.iter_commits(paths=File))
     print("Changes for %s:" % File)
-    LastCommit = AA[-1]
-    print(repo.git.diff(None, LastCommit, File))
+
+    latestCommit = commitsOnFile[-1]
+    print(repo.git.diff(None, latestCommit, File))
+
     MSG = input("Commit message for %s:\n" % File)
+
     repo.index.add([File])
     repo.index.commit(MSG)
