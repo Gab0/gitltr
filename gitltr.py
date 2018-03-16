b7ab0ad3efcf7e1aab370e4b24e4813a16501e35
Gab0 <gabriel_scf@hotmail.com>
2018-03-15 23:27:10 -0300

    removing useless lines

--- gitltr.py
+++ gitltr.py
@@ -4,16 +4,13 @@ import git
 
 GIT = git.cmd.Git('.')
 repo = git.Repo('.')
-files = GIT.ls_files()
-
-Q=repo.index.diff(repo.head.commit)
-C=repo.index.diff('HEAD')
-N=repo.head.commit.diff()
 
 FileDiffs = repo.index.diff(None)
 
 DIFFS = repo.git.diff(repo.head.commit.tree)
-#print(DIFFS)
+
+if not FileDiffs:
+    exit("Nothing to do.")
 
 for F in FileDiffs:
     File = F.b_path
